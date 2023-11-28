28766e192b4b65d51994ebc914931238a0493ac7
Navis Ayara <navisayara@gmail.com>
2023-11-28 17:58:44 +0300

    Add files via upload

---
+++ countdown_timer.py
@@ -0,0 +1,52 @@
+import time
+import threading
+from flet import Text
+
+def format_seconds(seconds):
+    minutes = seconds // 60
+    remaining_seconds = seconds % 60
+
+    return f"{minutes:02d}:{remaining_seconds:02d}"
+
+class CountdownTimer(Text):
+    def __init__(self,):
+        super().__init__()
+        self.seconds = 25*60
+        self.is_running = False
+        self.is_paused = False
+        self.timer_thread = None
+        self.value = format_seconds(self.seconds)
+        self.size = 144
+        self.color = "#ffffff"
+
+    def start(self):
+        if not self.is_running:
+            self.is_running = True
+            self.is_paused = False
+            self.timer_thread = threading.Thread(target=self._run_timer)
+            self.timer_thread.start()
+
+    def _run_timer(self):
+        while self.is_running and self.seconds > 0:
+            if not self.is_paused:
+                time.sleep(1)
+                self.seconds -= 1
+                self.value = format_seconds(self.seconds)
+                self.update()
+
+    def pause(self):
+        self.is_paused = True
+
+    def resume(self):
+        self.is_paused = False
+
+    def stop(self):
+        self.is_running = False
+        self.is_paused = False
+
+    def reset(self, new_seconds=25*60):
+        self.seconds = new_seconds
+        if not self.is_running:
+            self.value = format_seconds(self.seconds)
+            self.update()
+
---
+++ main.py
@@ -0,0 +1,214 @@
+import flet as ft
+from countdown_timer import CountdownTimer
+import os
+from math import pi
+
+images = []
+with os.scandir("assets/images") as entries:
+    for entry in entries:
+        if entry.is_file():
+            images.append(entry.name.removesuffix(".jpg"))
+
+
+def main(page: ft.Page):
+    page.title = "Focal"
+    page.padding = 0
+    page.window_title_bar_buttons_hidden = True
+    page.window_title_bar_hidden = True
+    page.theme = ft.Theme(color_scheme_seed="green")
+    page.window_min_height = 1000
+    page.window_min_width = 1000
+    page.window_full_screen = True
+    page.window_center()
+
+    def close_menu(e):
+        settings_menu.open = False
+        page.update()
+
+    def change_background(e):
+        app_container.image_src = f"/images/{background_picker.value}.jpg"
+        page.update()
+
+    background_picker = ft.Dropdown(
+        options=[ft.dropdown.Option(
+            image
+        ) for image in images],
+        width=270,
+        value=images[0],
+        on_change=change_background
+    )
+
+    title_bar = ft.WindowDragArea(
+        content=ft.Container(
+            alignment=ft.alignment.center_right,
+            padding=0,
+            expand=True,
+            height=28,
+            content=ft.IconButton(
+                icon=ft.icons.CLOSE_SHARP,
+                icon_size=14,
+                icon_color=ft.colors.WHITE,
+                style=ft.ButtonStyle(
+                    overlay_color=ft.colors.TRANSPARENT,
+                    bgcolor={"hovered": ft.colors.RED},
+                    shape=ft.RoundedRectangleBorder(radius=0)
+                ),
+                on_click=lambda _: page.window_close()
+            )
+        )
+    )
+
+    timer_setter = ft.TextField(
+        width=45,
+        value=25
+    )
+
+    def increment(e):
+        timer_setter.value += 5
+        timer_setter.update()
+
+    def decrement(e):
+        timer_setter.value -= 5
+        timer_setter.update()
+
+    settings_menu = ft.AlertDialog(
+        modal=True,
+        title=ft.Row([
+            ft.IconButton(
+                icon=ft.icons.CLOSE, 
+                icon_color="#ffffff", 
+                icon_size=25,
+                on_click=close_menu
+            ),
+            ft.Text("Settings", size=25)
+        ]),
+        content=ft.Container(
+            width=300,
+            height=300,
+            content=ft.Column([
+                ft.Text("Background image:"),
+                background_picker,
+                ft.Text("Timer Duration"),
+                ft.Row([
+                    timer_setter,
+                    ft.Column([
+                        ft.IconButton(icon=ft.icons.ADD, on_click=increment),
+                        ft.IconButton(icon=ft.icons.REMOVE, on_click=decrement)
+                    ])
+                ])
+            ])
+        )
+    )
+
+    def open_settings(e):
+        page.dialog = settings_menu
+        settings_menu.open = True
+        page.update()
+
+    settings_btn = ft.IconButton(
+        icon=ft.icons.SETTINGS_ROUNDED,
+        icon_color=ft.colors.WHITE,
+        style=ft.ButtonStyle(
+            surface_tint_color=ft.colors.TRANSPARENT,
+            bgcolor={"hovered": ft.colors.with_opacity(0.3, ft.colors.WHITE)}
+        ),
+        on_click=open_settings
+    )
+
+    timer = CountdownTimer()
+
+    def start_countdown(e):
+        timer.start()
+
+        start_btn.icon = ft.icons.STOP_ROUNDED
+        start_btn.text = "Stop"
+        start_btn.on_click = stop
+        page.update()
+
+    def pause_coutdown(e):
+        timer.pause()
+
+    #def resume_countdown(e):
+    #    timer.resume()
+
+    def reset(e):
+        restart_btn.rotate -= pi*2
+        timer.reset()
+        page.update()
+
+    def stop(e):
+        timer.stop()
+        start_btn.on_click=start_countdown
+        start_btn.icon = None
+        start_btn.text = "Start"
+        page.update()
+
+    pause_btn = ft.IconButton(
+        icon=ft.icons.PAUSE_ROUNDED,
+        icon_color=ft.colors.WHITE,
+        style=ft.ButtonStyle(
+            side=ft.BorderSide(1.5, "#ffffff")
+        ),
+        on_click=pause_coutdown
+    )
+
+    start_btn = ft.ElevatedButton(
+        icon_color=ft.colors.BLACK,
+        height=45,
+        text="Start",
+        style=ft.ButtonStyle(
+            bgcolor="#ffffff",
+            color=ft.colors.BLACK
+        ),
+        on_click=start_countdown
+    )
+
+    restart_btn = ft.IconButton(
+        icon=ft.icons.RESTART_ALT_ROUNDED,  
+        icon_color=ft.colors.WHITE,
+        style=ft.ButtonStyle(
+            side=ft.BorderSide(1.5, "#ffffff")
+        ),
+        rotate=0,
+        animate_rotation=ft.Animation(500, ft.AnimationCurve.EASE_IN_OUT),
+        on_click=reset
+    )
+
+    timer_controls = ft.Row([
+        pause_btn,
+        start_btn,
+        restart_btn
+    ], alignment=ft.MainAxisAlignment.CENTER)
+
+    app = ft.Container(
+        expand=True,
+        padding=25,
+        content=ft.Column([
+            ft.Row([
+                settings_btn
+            ], alignment=ft.MainAxisAlignment.END),
+            ft.Column([
+                ft.Row([
+                    ft.Stack([
+                        ft.Container(timer, alignment=ft.alignment.center),
+                    ], width=382, height=382)  
+                ], alignment=ft.MainAxisAlignment.CENTER),
+                timer_controls
+            ], spacing=40)
+        ])
+    )
+
+    app_container = ft.Container(
+        expand=True,
+        image_src="/images/Aerial Forest View.jpg",
+        image_fit=ft.ImageFit.COVER,
+        content=ft.Column([
+            title_bar,
+            app
+        ])
+    )
+
+    page.add(app_container)
+
+if __name__ == "__main__":
+    ft.app(target=main, assets_dir="assets")
